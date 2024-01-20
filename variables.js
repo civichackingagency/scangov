726cda03a36f1b183be4b984558880c6dfb235c9
Narlotl <luke@lukefretwell.com>
2024-01-19 19:58:40 -0800

    Remove tags
    keywords
    og:image:height
    og:image:width
    twitter:card
    twitter:title
    twitter:description
    twitter:image

--- variables.js
+++ variables.js
@@ -1,7 +1,6 @@
-const properties = ['<title', 'name="description"', 'name="keywords"', 'name="robots"', 'name="viewport"', 'rel="canonical"',
+const properties = ['<title', 'name="description"', 'name="robots"', 'name="viewport"', 'rel="canonical"',
     'property="og:locale"', 'property="og:site_name"', 'property="og:type"', 'property="og:title"', 'property="og:description"', 'property="og:url"',
-    'property="og:image"', 'property="og:image:width"', 'property="og:image:height"', 'property="og:image:alt"', 'name="twitter:card"',
-    'name="twitter:title"', 'name="twitter:description"', 'name="twitter:image"'];
+    'property="og:image"', 'property="og:image:alt"'];
 const variables = properties.map(property => {
     if (property.includes('"'))
         property = property.substring(property.indexOf('"') + 1, property.lastIndexOf('"'));
