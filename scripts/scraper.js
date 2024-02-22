60997c75dd987a903d2e83b092c7b7709448071a
Narlotl <fretwellcalifornia@gmail.com>
2024-02-21 16:32:40 -0800

    Add more tags, www resolution

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -6,15 +6,18 @@ import { exit } from 'process';
 const start = Date.now();
 
 // Tags to search for
-const properties = ['<title', 'name="description"', 'name="keywords"', 'name="robots"', 'name="viewport"', 'rel="canonical"',
+const properties = ['<title', '(<main)|(role="main")', 'name="description"', 'name="keywords"', 'name="robots"', 'name="viewport"', 'rel="canonical"',
     'property="og:locale"', 'property="og:site_name"', 'property="og:type"', 'property="og:title"', 'property="og:description"', 'property="og:url"',
-    'property="og:image"', 'property="og:image:width"', 'property="og:image:height"', 'property="og:image:alt"', 'name="twitter:site"', 'name="twitter:card"',
-    'name="twitter:title"', 'name="twitter:description"', 'name="twitter:image"']
+    'property="og:image"', 'property="og:image:width"', 'property="og:image:height"', 'property="og:image:alt"',
+    'dcterms.keywords', 'dc.subject', 'dcterms.subject', 'dcterms.audience', 'dc.type', 'dcterms.type', 'dc.date', 'dc.date.created', 'dcterms.created',
+    'name="article:section', 'name="article:tag', 'name="article:published_time', 'name="article:modified_time',
+    'name="twitter:site"', 'name="twitter:card"', 'name="twitter:title"', 'name="twitter:description"', 'name="twitter:image"']
     // Regular expressions for tags
-    .map(property => { return { string: property, regex: new RegExp(property.replaceAll('"', '("|)'), 'm') } });
+    .map(property => { return { string: property, regex: new RegExp(property.replaceAll('"', '("|)').replaceAll('.', '\\.'), 'm') } });
 // Their names in the code
 const variables = [
     'title',
+    'main',
     'description',
     'keywords',
     'robots',
@@ -30,6 +33,21 @@ const variables = [
     'ogImageWidth',
     'ogImageHeight',
     'ogImageAlt',
+    'dctermsKeywords',
+    'dcSubject',
+    'dctermsSubject',
+    'dctermsAudience',
+    'dcType',
+    'dctermsType',
+    'dcDate',
+    'dcDateCreated',
+    'dctermsCreated',
+    'articleSection',
+    'articleTag',
+    'articleSection',
+    'articleTag',
+    'articlePublishedTime',
+    'articleModifiedTime',
     'twitterSite',
     'twitterCard',
     'twitterTitle',
@@ -38,6 +56,7 @@ const variables = [
 ];
 const csvVariables = [
     'Title',
+    'Main',
     'Description',
     'Keywords',
     'Robots',
@@ -53,6 +72,20 @@ const csvVariables = [
     'OG Image Width',
     'OG Image Height',
     'OG Image Alt',
+    'DC Terms Keywords',
+    'DC Subject',
+    'DC Terms Subject',
+    'DC Terms Audience',
+    'DC Type',
+    'DC Terms Type',
+    'DC Date',
+    'DC Date Created',
+    'DC Terms Created',
+    'Article Section',
+    'Article Tag',
+    'Article Published Time',
+    'Article Modified Time',
+    'Twitter Site',
     'Twitter Card',
     'Twitter Title',
     'Twitter Description',
@@ -83,8 +116,7 @@ const fetchPromise = agency => {
         agencyData[0] = agencyData[0].toLowerCase();
 
         // Timeout requests
-        const controller = new AbortController();
-        const signal = controller.signal;
+        let controller = new AbortController();
         const timeout = setTimeout(() => {
             console.error('Aborting', agencyData[0]);
             clearTimeout(timeout);
@@ -93,7 +125,7 @@ const fetchPromise = agency => {
         }, 60000);
         const options = {
             method: 'GET',
-            signal,
+            signal: controller.signal,
             headers: {
                 'User-Agent': 'CivicHackingAgency/1.0 gov-metadata',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*\/*;q=0.8',
@@ -104,114 +136,32 @@ const fetchPromise = agency => {
         };
         let outcome;
         const fetchStart = Date.now();
-        fetch('http://' + agencyData[0], options).then(async res => {
-            const fetchEnd = Date.now();
-            let data = await res.text();
-            data = data.replaceAll('\'', '"').toLowerCase();
-            let url;
-
-            // Get data before update
-            const past = historyData.find(h => h.url == agencyData[0]);
-            let history = (past && past.history) ? past.history : [];
-            let oldData = { time: Date.now(), status: past ? past.status : null, /*sitemap: past ? past.sitemap : null,*/ dotgov: past ? past.dotgov : null, https: past ? past.https : null, redirect: past ? past.redirect : null, responseTime: past ? past.responseTime : null };
-            if (past)
-                for (let i = 0; i < variables.length; i++)
-                    oldData[variables[i]] = past[variables[i]];
-            let changed = false;
-
-            const redirect = url || res.url;
-            let tld = redirect.substring(redirect.indexOf('//') + 2);
-            tld = tld.substring(0, tld.indexOf('/'));
-            tld = tld.substring(tld.lastIndexOf('.') + 1);
-
-            //const baseUrl = redirect.substring(0, redirect.indexOf('/', redirect.indexOf('//') + 2));
-            //const sitemapReq = await fetch(baseUrl + '/sitemap.xml', options);
-            //const sitemap = res.status < 300 && sitemapReq.status < 300 && sitemapReq.url.includes('sitemap.xml');
-
-            // Initialize result
-            outcome = {
-                status: res.status,
-                url: agencyData[0],
-                name: capitalizeFirstLetters(agencyData[2]),
-                redirect,
-                dotgov: tld == 'gov',
-                https: res.url.startsWith('https://'),
-                responseTime: fetchEnd - fetchStart
-                //sitemap
-            };
-
-            // Check if the domain redirects in the http-equiv tag
-            let recursions = 0;
-            const checkForRefresh = async html => {
-                if (recursions >= 5) {
-                    for (const variable of variables)
-                        outcome[variable] = false;
-                    history.push(oldData);
-                    outcome.history = history;
-                }
-                else if (html.includes('http-equiv="refresh"')) {
-                    let index = html.indexOf('http-equiv="refresh"');
-                    index = html.indexOf('url=', index) + 4;
-                    let redirectUrl = html.substring(index);
-                    if (redirectUrl.startsWith('"'))
-                        redirectUrl = redirectUrl.substring(1);
-                    redirectUrl = redirectUrl.substring(0, redirectUrl.indexOf('"'));
-                    if (!redirectUrl.startsWith('//') && redirectUrl.startsWith('/'))
-                        redirectUrl = (url || ('http://' + agencyData[0])) + redirectUrl;
-                    url = redirectUrl;
-
-                    data = (await (await fetch(redirectUrl)).text()).replaceAll('\'', '"').toLowerCase();
-                    recursions++;
-                    await checkForRefresh(data);
-                }
-            }
-            await checkForRefresh(data);
-
-            // Check for properties
-            for (let i = 0; i < properties.length; i++) {
-                if (res.status == 200) {
-                    let index = data.match(properties[i].regex);
-                    if (!index) {
-                        outcome[variables[i]] = false;
-                        continue;
-                    }
-                    if (i == 0) {
-                        outcome[variables[i]] = true;
-                        continue;
-                    }
-                    index = index.index;
-                    let openIndex;
-                    for (let j = index; j >= 0; j--)
-                        if (data[j] == '<') {
-                            openIndex = j;
-                            break;
-                        }
-                    // Ensure the tag isn't blank
-                    const tag = data.substring(openIndex, data.indexOf('>', openIndex));
-                    const contentIndex = tag.indexOf((properties[i].string.includes('rel') ? 'href=' : 'content='));
-                    const contentLength = properties[i].string.includes('rel') ? 5 : 8;
-                    if (contentIndex == -1) {
-                        outcome[variables[i]] = false;
-                        continue;
-                    }
-                    const charAfter = tag.charAt(contentIndex + contentLength);
-                    outcome[variables[i]] = (charAfter == '"' && tag.charAt(contentIndex + contentLength + 1) != '"') || (charAfter != '"' && charAfter != ' ' && charAfter != '/' && charAfter != '>');
-                }
-                else
-                    outcome[variables[i]] = false;
-
-                if (!changed && oldData[variables[i]] != outcome[variables[i]])
-                    changed = true;
-            }
-
-            // Push to history
-            if (changed && past)
-                history.push(oldData);
-            outcome.history = history;
+        const final = () => {
             outcomes.push(outcome);
-        }).catch(err => {
+
+            done++;
+            let total = 0;
+            for (let i = 0; i < variables.length; i++)
+                if (outcome[variables[i]])
+                    total++;
+            console.log(`Done with ${agencyData[0]} (${outcome.status}, ${total}/${variables.length}) ${done}/${promises.length} ${Math.round(done / promises.length * 100)}% in ${(Math.round((Date.now() - start) / 1000 / 60)).toString().padStart(2, '0')}:${(Math.round((Date.now() - start) / 1000) % 60).toString().padStart(2, '0')}`);
+
+            resolve();
+        };
+        let errorCount = 0;
+        const catchError = err => {
+            errorCount++;
             const status = (err.name === 'AbortError' ? 408 : 500);
-            outcome = { status, url: agencyData[0], name: capitalizeFirstLetters(agencyData[2]) };
+            outcome = {
+                status,
+                url: agencyData[0],
+                www: false,
+                name: capitalizeFirstLetters(agencyData[2]),
+                redirect: null,
+                dotgov: agencyData[0].endsWith('.gov'),
+                https: false,
+                responseTime: Date.now() - fetchStart
+            };
             const past = historyData.find(h => h.url == agencyData[0]);
             let history = (past && past.history) ? past.history : [];
             let oldData = { time: Date.now(), status: past ? past.status : null, /*sitemap: past ? past.sitemap : null,*/ dotgov: past ? past.dotgov : null, https: past ? past.https : null, redirect: past ? past.redirect : null, responseTime: past ? past.responseTime : null };
@@ -223,29 +173,175 @@ const fetchPromise = agency => {
             outcome.history = history;
             for (let i = 0; i < properties.length; i++)
                 outcome[variables[i]] = false;
-            outcomes.push(outcome);
 
             try {
                 errors.push({ agencyData, error: err.name + ': ' + err.message });
                 JSON.stringify(errors);
             }
-            catch (err) {
+            catch (e) {
                 errors.pop();
             }
 
-            console.error(agencyData[0], err);
-        }).finally(() => {
-            clearTimeout(timeout);
+            if (errorCount == 2) {
+                console.error(agencyData[0], err);
+                final();
+            }
+        };
+        const nonWww = await fetch('http://' + agencyData[0], options).catch(catchError);
+        const www = await fetch('http://www.' + agencyData[0], options).catch(catchError);
+        const domainPromises = [nonWww, www];
+        await Promise.all(domainPromises);
+        let res;
+        if (nonWww && nonWww.status == 200)
+            res = nonWww;
+        else if (www && www.status == 200)
+            res = www;
+        else
+            catchError({ name: 'UnknownError', message: 'Unknown error' });
 
-            done++;
-            let total = 0;
+        const fetchEnd = Date.now();
+        let data = await res.text();
+        data = data.replaceAll('\'', '"').toLowerCase();
+
+        // Get data before update
+        const past = historyData.find(h => h.url == agencyData[0]);
+        let history = (past && past.history) ? past.history : [];
+        let oldData = { time: Date.now(), status: past ? past.status : null, /*sitemap: past ? past.sitemap : null,*/ dotgov: past ? past.dotgov : null, https: past ? past.https : null, redirect: past ? past.redirect : null, responseTime: past ? past.responseTime : null };
+        if (past)
             for (let i = 0; i < variables.length; i++)
-                if (outcome[variables[i]])
-                    total++;
-            console.log(`Done with ${agencyData[0]} (${outcome.status}, ${total}/${variables.length}) ${done}/${promises.length} ${Math.round(done / promises.length * 100)}% in ${(Math.round((Date.now() - start) / 1000 / 60)).toString().padStart(2, '0')}:${(Math.round((Date.now() - start) / 1000) % 60).toString().padStart(2, '0')}`);
+                oldData[variables[i]] = past[variables[i]];
+        let changed = false;
 
-            resolve();
-        });
+        const redirect = res.url;
+        let tld = redirect.substring(redirect.indexOf('//') + 2);
+        tld = tld.substring(0, tld.indexOf('/'));
+        tld = tld.substring(tld.lastIndexOf('.') + 1);
+        console.log(redirect)
+
+        //const baseUrl = redirect.substring(0, redirect.indexOf('/', redirect.indexOf('//') + 2));
+        //const sitemapReq = await fetch(baseUrl + '/sitemap.xml', options);
+        //const sitemap = res.status < 300 && sitemapReq.status < 300 && sitemapReq.url.includes('sitemap.xml');
+
+        // Initialize result
+        outcome = {
+            status: res.status,
+            url: agencyData[0],
+            www: nonWww && nonWww.status == 200 && www && www.status == 200 && nonWww.url == www.url,
+            name: capitalizeFirstLetters(agencyData[2]),
+            redirect,
+            dotgov: tld == 'gov',
+            https: res.url.startsWith('https://'),
+            responseTime: fetchEnd - fetchStart
+            //sitemap
+        };
+
+        // Check if the domain redirects in the http-equiv tag
+        let recursions = 0;
+        const checkForRefresh = async html => {
+            if (recursions >= 5) {
+                for (const variable of variables)
+                    outcome[variable] = false;
+                history.push(oldData);
+                outcome.history = history;
+            }
+            else if (html.includes('http-equiv="refresh"')) {
+                let index = html.indexOf('http-equiv="refresh"');
+                index = html.indexOf('url=', index) + 4;
+                let redirectUrl = html.substring(index);
+                if (redirectUrl.startsWith('"'))
+                    redirectUrl = redirectUrl.substring(1);
+                redirectUrl = redirectUrl.substring(0, redirectUrl.indexOf('"'));
+                if (!redirectUrl.startsWith('//') && redirectUrl.startsWith('/'))
+                    redirectUrl = (url || ('http://' + agencyData[0])) + redirectUrl;
+                url = redirectUrl;
+
+                data = (await (await fetch(redirectUrl)).text()).replaceAll('\'', '"').toLowerCase();
+                recursions++;
+                await checkForRefresh(data);
+            }
+        }
+        await checkForRefresh(data);
+
+        // Check for properties
+        for (let i = 0; i < properties.length; i++) {
+            if (res.status == 200) {
+                let index = data.match(properties[i].regex);
+                if (!index) {
+                    outcome[variables[i]] = false;
+                    continue;
+                }
+                if (i == 0 || i == 1) {
+                    outcome[variables[i]] = true;
+                    continue;
+                }
+                index = index.index;
+                let openIndex;
+                for (let j = index; j >= 0; j--)
+                    if (data[j] == '<') {
+                        openIndex = j;
+                        break;
+                    }
+                // Ensure the tag isn't blank
+                const tag = data.substring(openIndex, data.indexOf('>', openIndex));
+                const contentIndex = tag.indexOf((properties[i].string.includes('rel') ? 'href=' : 'content='));
+                const contentLength = properties[i].string.includes('rel') ? 5 : 8;
+                if (contentIndex == -1) {
+                    outcome[variables[i]] = false;
+                    continue;
+                }
+                const charAfter = tag.charAt(contentIndex + contentLength);
+                outcome[variables[i]] = (charAfter == '"' && tag.charAt(contentIndex + contentLength + 1) != '"') || (charAfter != '"' && charAfter != ' ' && charAfter != '/' && charAfter != '>');
+            }
+            else
+                outcome[variables[i]] = false;
+
+            if (!changed && oldData[variables[i]] != outcome[variables[i]])
+                changed = true;
+        }
+
+        // Push to history
+        if (changed && past)
+            history.push(oldData);
+        outcome.history = history;
+
+        final();
+        /*}).catch(err => {
+            const status = (err.name === 'AbortError' ? 408 : 500);
+            outcome = { status, url: agencyData[0], name: capitalizeFirstLetters(agencyData[2]) };
+            const past = historyData.find(h => h.url == agencyData[0]);
+            let history = (past && past.history) ? past.history : [];
+            let oldData = { time: Date.now(), status: past ? past.status : null, /*sitemap: past ? past.sitemap : null,*//* dotgov: past ? past.dotgov : null, https: past ? past.https : null, redirect: past ? past.redirect : null, responseTime: past ? past.responseTime : null };
+if (past)
+for (let i = 0; i < variables.length; i++)
+oldData[variables[i]] = past[variables[i]];
+if (status != past.status)
+history.push(oldData);
+outcome.history = history;
+for (let i = 0; i < properties.length; i++)
+outcome[variables[i]] = false;
+outcomes.push(outcome);
+
+try {
+errors.push({ agencyData, error: err.name + ': ' + err.message });
+JSON.stringify(errors);
+}
+catch (err) {
+errors.pop();
+}
+
+console.error(agencyData[0], err);
+}).finally(() => {
+clearTimeout(timeout);
+
+done++;
+let total = 0;
+for (let i = 0; i < variables.length; i++)
+if (outcome[variables[i]])
+total++;
+console.log(`Done with ${agencyData[0]} (${outcome.status}, ${total}/${variables.length}) ${done}/${promises.length} ${Math.round(done / promises.length * 100)}% in ${(Math.round((Date.now() - start) / 1000 / 60)).toString().padStart(2, '0')}:${(Math.round((Date.now() - start) / 1000) % 60).toString().padStart(2, '0')}`);
+
+resolve();
+});*/
     })
 };
 
@@ -353,11 +449,11 @@ console.log('Done fetching');
 
 writeFileSync('data/data.json', JSON.stringify(outcomes));
 
-let csv = 'Domain,Redirect,Agency,Status,Response Time,' + csvVariables.join(',') + '\n';
-for (const agency of outcomes) {
-    csv += agency.url + ',' + agency.redirect + ',' + agency.name + ',' + agency.status + ',' + agency.responseTime;
+let csv = 'Domain,Redirect,HTTPS,.gov,www,Agency,Status,Response Time,' + csvVariables.join(',') + '\n';
+for (const domain of outcomes) {
+    csv += domain.url + ',' + domain.redirect + ',' + domain.https + ',' + domain.dotgov + ',' + domain.www + ',' + domain.name + ',' + domain.status + ',' + domain.responseTime;
     for (let i = 0; i < variables.length; i++)
-        csv += ',' + agency[variables[i]];
+        csv += ',' + domain[variables[i]];
     csv += '\n';
 }
 writeFileSync('data/data.csv', csv);
