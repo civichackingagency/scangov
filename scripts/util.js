98541185f4b6987b33c9c6e09ddfa00e33b63fa7
Elias Fretwell <fretwellcalifornia@gmail.com>
2025-01-22 10:38:39 -0800

    Don't use cache API if it isn't available

--- scripts/util.js
+++ scripts/util.js
@@ -20,37 +20,51 @@ const getColor = score => {
 
 let CHECK_WWW = true;
 
-let cache;
+let cache, updateTime, updateTimeReq;
 const getData = async (file) => {
-    if (!cache)
-        cache = await caches.open('data');
-
-    const cacheTime = await getCacheTime();
-    const updateTime = new Date(parseInt(await (await fetch('/data/updated_time')).text()));
+    if (!updateTimeReq) {
+        updateTimeReq = fetch('/data/updated_time').then(res=>res.text());
+        updateTime = new Date(parseInt(await updateTimeReq));
+    }
+    else
+        await updateTimeReq;
     if (document.getElementById('updated'))
         document.getElementById('updated').innerText = updateTime.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' });
 
-    if (cacheTime < updateTime.getTime()) {
-        // New data, clear cached data
+    if (caches) {
+        // Cache API is available
         if (!cache)
             cache = await caches.open('data');
 
-        const promises = (await cache.keys()).map(key => cache.delete(key));
+        const cacheTime = await getCacheTime();
 
-        await Promise.all(promises);
+        if (cacheTime < updateTime.getTime()) {
+            // New data, clear cached data
+            if (!cache)
+                cache = await caches.open('data');
+
+            const promises = (await cache.keys()).map(key => cache.delete(key));
+
+            await Promise.all(promises);
+        }
+
+
+        file = '/data/' + file + '.json';
+
+        let data = await cache.match(file);
+        if (!data) {
+            // Data isn't cached, make a request and store it
+            await cache.add(file);
+            await cache.put('/timestamp', new Response(Date.now()));
+            data = await cache.match(file);
+        }
+
+        return data.json();
     }
-
-
-    file = '/data/' + file + '.json';
-
-    let data = await cache.match(file);
-    if (!data) {
-        await cache.add(file);
-        await cache.put('/timestamp', new Response(Date.now()));
-        data = await cache.match(file);
+    else {
+        // Cache API isn't available so just make a request
+        return await (await fetch('/data/' + file + '.json')).json();
     }
-
-    return data.json();
 };
 
 const getCacheTime = async () => {
